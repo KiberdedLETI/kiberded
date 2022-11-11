521ed3255568b559337b9619cf614e8207678154
Evgeniy Setrov <evgensetrov@gmail.com>
2022-11-11 03:03:24 +0300

    feat: сервис для запуска веб-деда

--- update/update_daemon_starter.py
+++ update/update_daemon_starter.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", log_level="info")
+    uvicorn.run("app:app", host="0.0.0.0", port=80, reload=True, log_level="info")
