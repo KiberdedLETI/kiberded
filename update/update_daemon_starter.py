63afa90a5ca311f88714ba1082b745ea558e9f18
Evgeniy Setrov <77460785+evgensetrov@users.noreply.github.com>
2022-11-12 17:34:20 +0300

    fix: поменял порт

--- update/update_daemon_starter.py
+++ update/update_daemon_starter.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run("app:app",
                 host="0.0.0.0",
-                port=80,
+                port=443,
                 reload=True,
                 log_level="info",
                 ssl_keyfile='/etc/letsencrypt/live/kiberded.ga/privkey.pem',
